c60214cd92b6996bab20b9c83f7223c440edcea6
Aaron Dutta <aaronsumit123@gmail.com>
2026-03-08 07:24:55 -0400

    myhub in progress

--- app/frontend/src/App.tsx
+++ app/frontend/src/App.tsx
@@ -3,6 +3,7 @@ import { NodeIdProvider } from "@/context/NodeIdContext";
 import Homepage from "@/pages/Homepage";
 import MyFarmPage from "@/pages/MyFarmPage";
 import MyFoodPage from "@/pages/MyFoodPage";
+import AdminDashboard from "@/AdminDashboard";
 
 export default function App() {
   return (
@@ -12,6 +13,7 @@ export default function App() {
           <Route path="/" element={<Homepage />} />
           <Route path="/myfarm" element={<MyFarmPage />} />
           <Route path="/myfood" element={<MyFoodPage />} />
+          <Route path="/admin" element={<AdminDashboard />} />
         </Routes>
       </BrowserRouter>
     </NodeIdProvider>
